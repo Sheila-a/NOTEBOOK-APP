03bef5e34dcde9ff1b1e4331dbbc5aceb6aa4ca8
Sheila-a <ugwuodo20@gmail.com>
2023-02-27 21:26:14 +0100

    Final Test 002

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Burger } from "../Hamburger/Hamburger.js";
 import design from "./Header.module.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className={design.header}>
-      <h1>Sheila's Notes</h1>
+      <h1>
+        <Link to="/edit-notes"> Sheila's Notes</Link>
+      </h1>
       <Burger />
     </div>
   );
